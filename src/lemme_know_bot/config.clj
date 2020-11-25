2fd5f4e076c91bce4a12661e02b1dab47a71a4ac
wdhowe <wdhowe@protonmail.com>
2020-11-25 17:34:23 -0600

    issue-1: Search list not persistent
    New functions for loading an edn file and saving any generic file.

--- src/lemme_know_bot/config.clj
+++ src/lemme_know_bot/config.clj
@@ -1,11 +1,36 @@
 (ns lemme-know-bot.config
   (:gen-class)
-  (:require [environ.core :as environ]))
+  (:require [clojure.edn :as edn]
+            [environ.core :as environ]
+            [clojure.java.io :as io]
+            [taoensso.timbre :as log]))
 
 (def config
   {:timeout (or (environ/env :lkb-timeout) 10) ; timeout in seconds to wait for long polling
    :sleep (or (environ/env :lkb-sleep) 10000); sleep in milliseconds between polls
-   })
+   :searches (or (environ/env :lkb-searches) "/tmp/lemme-know-bot-searches.edn")})
 
 (comment
   (println "config is:" config))
+
+(defn load-edn
+  "Load edn files from an io/reader `source` (filename or io/resource).
+   Orig fn: https://clojuredocs.org/clojure.edn/read#example-5a68f384e4b09621d9f53a79"
+  [source]
+  (try
+    (with-open [r (io/reader source)]
+      (edn/read (java.io.PushbackReader. r)))
+
+    (catch java.io.IOException e
+      (log/errorf "couldn't open '%s': %s" source (.getMessage e)))
+    (catch RuntimeException e
+      (log/errorf "error parsing edn file '%s': %s" source (.getMessage e)))))
+
+(defn save-file
+  "Save a data structure to a file."
+  [content dest]
+  (try
+    (spit dest content)
+
+    (catch java.io.IOException e
+      (log/errorf "couldn't write to '%s': %s" dest (.getMessage e)))))
