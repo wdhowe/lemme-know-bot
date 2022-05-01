b914cd45b9c8a69abc7a3951bba28f58a5e0eec4
Bill Howe <wdhowe@protonmail.com>
2022-04-30 20:07:02 -0500

    Bump dependency versions.

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject lemme-know-bot "0.5.0"
+(defproject lemme-know-bot "0.6.0"
   :description "A Telegram Bot that mentions you when the specified text in a chat matches."
   :url "https://github.com/wdhowe/lemme-know-bot"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[com.taoensso/timbre "5.1.2"]
+  :dependencies [[com.taoensso/timbre "5.2.1"]
                  [environ "1.2.0"]
-                 [org.clojure/clojure "1.10.3"]
-                 [telegrambot-lib "1.3.0"]
+                 [org.clojure/clojure "1.11.1"]
+                 [telegrambot-lib "2.0.0"]
                  [cheshire "5.10.2"]]
   :main ^:skip-aot lemme-know-bot.core
   :target-path "target/%s"
